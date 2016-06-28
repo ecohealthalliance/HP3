41288994520db30a70e0c78b469fb98e1b370c9f
Noam Ross <noam.ross@gmail.com>
2016-06-28 12:23:26 -0400

    Calculate deviance explained with offset (82%)

---
+++ misc/zoonotic_dev_explained_w_offset.R
@@ -0,0 +1,11 @@
+P <- rprojroot::find_rstudio_root_file
+library(mgcv)
+
+# Calculate the deviance explained when including the offset of the zoonotic model
+# Normally null deviance is calculated using the offset, so here we do it manually
+
+zoo_model <- readRDS(P("supplement/all_zoonoses_model.rds"))
+null_model <- gam(formula = NSharedWithHoSa ~ 1, data=zoo_model$model, family=poisson)
+(null_model$deviance -  zoo_model$deviance) / null_model$deviance
+
+#result (2016-06-28): 0.8198646

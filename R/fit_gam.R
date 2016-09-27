e8a0d71a2fc52122791d737711b8b6822c35d34c
Noam Ross <noam.ross@gmail.com>
2016-09-27 15:29:12 -0400

    Fix fit_gam to comply with dplyr 0.5.0

--- R/fit_gam.R
+++ R/fit_gam.R
@@ -1,66 +1,66 @@
 fit_all_gams <- function(data_set, outcome_variable, model_family, terms) {
-  
+
   fit_gam = function(frm) {
     try(gam(formula=as.formula(frm), model_family, data_set, select=TRUE), silent=TRUE)
   }
-  
+
   terms_grid = do.call(expand.grid, terms)
-  
+
   #Create model forumulas from the grid
-  formulas = apply(as.matrix(terms_grid), 1, function(row) paste(row, collapse = " + ")) %>% 
-    stri_replace_all_regex("\\s[\\+\\s]+\\s", " + ") %>% 
-    {paste(outcome_variable, "~", .)} %>% 
-    rearrange_formula %>% 
+  formulas = apply(as.matrix(terms_grid), 1, function(row) paste(row, collapse = " + ")) %>%
+    stri_replace_all_regex("\\s[\\+\\s]+\\s", " + ") %>%
+    {paste(outcome_variable, "~", .)} %>%
+    rearrange_formula %>%
     unique
-  
+
   models = data_frame(formula = formulas)
-  
+
   n_cores = detectCores()
   n_cores_use = round(nrow(models) / ceiling(nrow(models) / (n_cores - 1)))
   options(mc.cores = n_cores_use)
   message("Using ", n_cores_use, " cores to fit ", nrow(models), " models")
-  
-  models_vec = mclapply(models$formula, fit_gam) 
-  
-  models = models %>% 
-    mutate(model = models_vec)  
-  
+
+  models_vec = mclapply(models$formula, fit_gam)
+
+  models = models %>%
+    mutate(model = models_vec)
+
   # Calculate models
-  models = models %>% 
-    filter(map_lgl(model, ~ !("try-error" %in% class(.) | is.null(.)))) %>% 
+  models = models %>%
+    filter(map_lgl(model, ~ !("try-error" %in% class(.) | is.null(.)))) %>%
     mutate(aic = map_dbl(model, MuMIn::AICc),
            daic = aic - min(aic),
-           weight = exp(-daic/2)) %>% 
+           weight = exp(-daic/2)) %>%
     arrange(aic)
-  
+
   # Remove unused terms from models and reduce to unique ones
-  models_reduced = models %>% 
-    select(model) %>% 
-    mutate(formula = map_chr(model, ~rearrange_formula(rm_low_edf(.)))) %>% 
-    distinct(formula)
-  
+  models_reduced = models %>%
+    select(model) %>%
+    mutate(formula = map_chr(model, ~rearrange_formula(rm_low_edf(.)))) %>%
+    distinct(formula, .keep_all = TRUE)
+
   n_cores_use = round(nrow(models_reduced) / ceiling(nrow(models_reduced) / (n_cores - 1)))
   options(mc.cores = n_cores_use)
   message("Using ", n_cores_use, " cores to fit ", nrow(models_reduced), " reduced models")
-  
+
   # Reduce the remaining models
-  models_reduced = models_reduced %>% 
+  models_reduced = models_reduced %>%
     mutate(model = mclapply(model, reduce_model))
-  
-  
-  models_reduced = models_reduced %>% 
-    filter(map_lgl(model, ~ !("try-error" %in% class(.) | is.null(.)))) %>% 
-    mutate(aic = map_dbl(model, MuMIn::AICc)) %>% 
-    mutate(formula = map_chr(model, ~rearrange_formula(rm_low_edf(.)))) %>% 
-    distinct(formula) %>% 
-    arrange(aic) %>% 
+
+
+  models_reduced = models_reduced %>%
+    filter(map_lgl(model, ~ !("try-error" %in% class(.) | is.null(.)))) %>%
+    mutate(aic = map_dbl(model, MuMIn::AICc)) %>%
+    mutate(formula = map_chr(model, ~rearrange_formula(rm_low_edf(.)))) %>%
+    distinct(formula, .keep_all=TRUE) %>%
+    arrange(aic) %>%
     mutate(daic = aic - min(aic),
            weight = exp(-daic/2),
            terms = shortform(map(model, ~ rearrange_formula(.$formula))),
            relweight = ifelse(daic > 2, 0, weight/sum(weight[daic < 2])),
            relweight_all = weight/sum(weight),
            cumweight = cumsum(relweight_all))
-  
+
   return(models_reduced)
-  
+
 }
